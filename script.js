81590879ea643107620ae233a2f10288848f2242
myexistance <joonyoonlives@gmail.com>
2019-02-02 15:37:11 +1300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    items: [],
+    currentValue: ''
+  },
+  methods: {
+    add() {
+      this.items.push(this.currentValue);
+      this.currentValue = '';
+    },
+    remove(itemIndex) {
+      this.items.splice(itemIndex, 1);
+    }
+  }
+});
